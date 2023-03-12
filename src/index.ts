0083df4fb533c6103d5e5e5422c1dfba9c258b01
Mark Dickie <mdickie1@gmail.com>
2023-03-12 15:17:05 +1100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ import { createQueue, setupQueueProcessor } from './queue';
 
 const connection = {
   host: env.REDISHOST,
-  port: env.REDISPORT ? parseInt(env.REDISPORT) : 6379,
+  port: env.REDISPORT,
   username: env.REDISUSER,
   password: env.REDISPASSWORD,
 };
