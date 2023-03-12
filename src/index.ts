a6679aed9e99518e77986c2cf84875109bc980e3
Mark Dickie <mdickie1@gmail.com>
2023-03-12 15:15:20 +1100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { createBullBoard } from '@bull-board/api';
+import { Queue } from 'bullmq';
 import { BullMQAdapter } from '@bull-board/api/bullMQAdapter';
 import { FastifyAdapter } from '@bull-board/fastify';
 import fastify, { FastifyInstance, FastifyRequest } from 'fastify';
@@ -7,68 +8,81 @@ import { env } from './env';
 
 import { createQueue, setupQueueProcessor } from './queue';
 
-interface AddJobQueryString {
-  id: string;
-  email: string;
-}
+// interface AddJobQueryString {
+//   id: string;
+//   email: string;
+// }
+
+const connection = {
+  host: env.REDISHOST,
+  port: env.REDISPORT ? parseInt(env.REDISPORT) : 6379,
+  username: env.REDISUSER,
+  password: env.REDISPASSWORD,
+};
 
 const run = async () => {
-  const priceQueue = createQueue('PriceQueue');
+  //   const priceQueue = createQueue('PriceQueue');
+  const priceQueue = new Queue('PriceQueue', {
+    connection,
+  });
   await setupQueueProcessor(priceQueue.name);
 
-  const server: FastifyInstance<Server, IncomingMessage, ServerResponse> =
-    fastify();
+  // const server: FastifyInstance<
+  //   Server,
+  //   IncomingMessage,
+  //   ServerResponse
+  // > = fastify();
 
-  const serverAdapter = new FastifyAdapter();
+  // const serverAdapter = new FastifyAdapter();
   createBullBoard({
     queues: [new BullMQAdapter(priceQueue)],
-    serverAdapter,
+    //     serverAdapter,
   });
-  serverAdapter.setBasePath('/');
-  server.register(serverAdapter.registerPlugin(), {
-    prefix: '/',
-    basePath: '/',
-  });
+  //   serverAdapter.setBasePath('/');
+  //   server.register(serverAdapter.registerPlugin(), {
+  //     prefix: '/',
+  //     basePath: '/',
+  //   });
 
-  server.get(
-    '/add-job',
-    {
-      schema: {
-        querystring: {
-          type: 'object',
-          properties: {
-            title: { type: 'string' },
-            id: { type: 'string' },
-          },
-        },
-      },
-    },
-    (req: FastifyRequest<{ Querystring: AddJobQueryString }>, reply) => {
-      if (
-        req.query == null ||
-        req.query.email == null ||
-        req.query.id == null
-      ) {
-        reply
-          .status(400)
-          .send({ error: 'Requests must contain both an id and a email' });
+  //   server.get(
+  //     '/add-job',
+  //     {
+  //       schema: {
+  //         querystring: {
+  //           type: 'object',
+  //           properties: {
+  //             title: { type: 'string' },
+  //             id: { type: 'string' },
+  //           },
+  //         },
+  //       },
+  //     },
+  //     (req: FastifyRequest<{ Querystring: AddJobQueryString }>, reply) => {
+  //       if (
+  //         req.query == null ||
+  //         req.query.email == null ||
+  //         req.query.id == null
+  //       ) {
+  //         reply
+  //           .status(400)
+  //           .send({ error: 'Requests must contain both an id and a email' });
 
-        return;
-      }
+  //         return;
+  //       }
 
-      const { email, id } = req.query;
-      welcomeEmailQueue.add(`WelcomeEmail-${id}`, { email });
+  //       const { email, id } = req.query;
+  //       priceQueue.add(`price-${id}`, { email });
 
-      reply.send({
-        ok: true,
-      });
-    }
-  );
+  //       reply.send({
+  //         ok: true,
+  //       });
+  //     }
+  //   );
 
-  await server.listen({ port: env.PORT, host: '0.0.0.0' });
-  console.log(
-    `To populate the queue and demo the UI, run: curl https://${env.RAILWAY_STATIC_URL}/add-job?id=1&email=hello%40world.com`
-  );
+  // await server.listen({ port: env.PORT, host: '0.0.0.0' });
+  // console.log(
+  //   `To populate the queue and demo the UI, run: curl https://${env.RAILWAY_STATIC_URL}/add-job?id=1&email=hello%40world.com`
+  // );
 };
 
 run().catch((e) => {
