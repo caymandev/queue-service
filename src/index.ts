e9ea87e3fed175cfbb96096432944e74c875de70
blackfry <mdickie1@gmail.com>
2023-03-15 18:05:02 +1100

    update packages

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
 require('dotenv').config();
-const { createBullBoard } = require('@bull-board/api');
+const { createBullBoard, IServerAdapter } = require('@bull-board/api');
 const { BullMQAdapter } = require('@bull-board/api/bullMQAdapter');
 const { ExpressAdapter } = require('@bull-board/express');
 const { Queue: QueueMQ, Worker: WorkerMQ, QueueScheduler } = require('bullmq');
 const express = require('express');
-
-const sleep = (t) => new Promise((resolve) => setTimeout(resolve, t * 1000));
 
 const redisOptions = {
   host: process.env.REDISHOST,
@@ -17,29 +15,10 @@ const redisOptions = {
 
 const createQueueMQ = (name) => new QueueMQ(name, { connection: redisOptions });
 
-async function setupBullMQProcessor(queueName) {
-  const queueScheduler = new QueueScheduler(queueName, {
-    connection: redisOptions,
-  });
-  await queueScheduler.waitUntilReady();
-
-  new WorkerMQ(queueName, async (job) => {
-    for (let i = 0; i <= 100; i++) {
-      await sleep(Math.random());
-      await job.updateProgress(i);
-      await job.log(`Processing job at interval ${i}`);
-
-      if (Math.random() * 200 < 1) throw new Error(`Random error ${i}`);
-    }
-
-    return { jobId: `This is the return value of job (${job.id})` };
-  });
-}
-
 const run = async () => {
   const priceBullMq = createQueueMQ('PriceQueue');
 
-  await setupBullMQProcessor(priceBullMq.name);
+  // await setupBullMQProcessor(priceBullMq.name);
 
   const app = express();
   // Configure view engine to render EJS templates.
@@ -71,20 +50,20 @@ const run = async () => {
     });
   });
 
-  app.listen(process.env.PORT, '0.0.0.0', () => {
-    // console.log('Running on 3000...');
-    // console.log(
-    //   'For the UI of instance1, open http://localhost:3000/instance1'
-    // );
-    // console.log(
-    //   'For the UI of instance2, open http://localhost:3000/instance2'
-    // );
-    // console.log('Make sure Redis is running on port 6379 by default');
-    // console.log('To populate the queue, run:');
-    // console.log('  curl http://localhost:3000/add?title=Example');
-    // console.log('To populate the queue with custom options (opts), run:');
-    // console.log('  curl http://localhost:3000/add?title=Test&opts[delay]=9');
-  });
+  // app.listen(process.env.PORT, '0.0.0.0', () => {
+  //   // console.log('Running on 3000...');
+  //   // console.log(
+  //   //   'For the UI of instance1, open http://localhost:3000/instance1'
+  //   // );
+  //   // console.log(
+  //   //   'For the UI of instance2, open http://localhost:3000/instance2'
+  //   // );
+  //   // console.log('Make sure Redis is running on port 6379 by default');
+  //   // console.log('To populate the queue, run:');
+  //   // console.log('  curl http://localhost:3000/add?title=Example');
+  //   // console.log('To populate the queue with custom options (opts), run:');
+  //   // console.log('  curl http://localhost:3000/add?title=Test&opts[delay]=9');
+  // });
 };
 
 // eslint-disable-next-line no-console
